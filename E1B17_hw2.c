9f4c7e37f445b2333a3a687f6925e16563c9cf44
Kuan-kai <u11302217@o363.ttu.edu.tw>
2025-04-17 22:36:56 +0800

    輸入第二題的直角三角形

--- E1B17_hw2.c
+++ E1B17_hw2.c
@@ -2,6 +2,8 @@
 #include<stdlib.h>
 int main(void){
 	int i,j,a,b=0;
+	char c,e,space,ch,d;
+	
 	for(i=1;i<=9;i++){
 		for(j=1;j<=i;j++){
 		printf("%d",j);	
@@ -25,11 +27,39 @@ int main(void){
 		printf("請輸入密碼:");
 		scanf("%d",&a);
 		if(a==2025)
+			
 			break;
 		else
 			b++;
 	}
 	system("CLS");
+	if(a==2025){
+		printf("__________________\n"); 
+		printf("|a.畫出直角三角形|\n");
+		printf("|b.顯示乘法表    |\n");
+		printf("|c.結束          |\n");
+		printf("__________________\n"); 
+	}
+	fflush(stdin);
+	scanf("%c",&e);
+	if(e=='a'&&'A')
+		system("CLS");
+		printf("請輸入一個a到n的字元");
+		fflush(stdin);
+		scanf("%c",&c) ;
+		//印三角形 
+		for (d=c;d>='a';d--) {
+        // 印空格
+        for (space='a'; space<d;space++) {
+            printf(" ");
+        }
+        // 印字母
+        for (ch=d;ch<=c;ch++) {
+            printf("%c", ch);
+        }
+        printf("\n");
+    }
 	system("pause");
+	
 	return 0;
 }

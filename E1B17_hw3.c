c42fc0a538d942c5a76cd926c50f609dfc7fcb83
Kuan-kai <u11302217@o363.ttu.edu.tw>
2025-05-02 19:32:30 +0800

    完成第一大題(個人面板及密碼)

---
+++ E1B17_hw3.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void){
+	//«ֵ§iֵÜ¼ֶ 
+	int i,j,a,b=0,g,h,k;
+	char c,e,space,ch,d,l;
+	//¨ד¦³¦U₪H­·®ז×÷µe­± 
+	printf("שÝשששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששß\n");
+	printf("שר                                                    שר\n");
+	printf("שר    ¢i¢i¢i¢i¢i ¢i¢i¢i¢i¢i ¢i¢i¢i¢i¢i ¢i¢i¢i¢i¢i                         שר\n");
+    printf("שר        ¢i ¢i   ¢i     ¢i ¢i                             שר\n");
+    printf("שר    ¢i¢i¢i¢i¢i ¢i   ¢i ¢i¢i¢i¢i¢i ¢i¢i¢i¢i¢i                         שר\n");
+    printf("שר    ¢i     ¢i   ¢i ¢i         ¢i                         שר\n");
+    printf("שר    ¢i¢i¢i¢i¢i ¢i¢i¢i¢i¢i ¢i¢i¢i¢i¢i ¢i¢i¢i¢i¢i                         שר\n");
+	printf("שר                                                    שר\n");
+	printf("שר                                                    שר\n");
+	printf("שר       ֵw×ן¨׃¨לE1B17×÷µ{¦¡³]­p                      שר\n");
+	printf("שר                               -µ{¦¡§@·~3           שר\n");
+	printf("שר                                                    שר\n");
+	printf("שר^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^שר\n");
+	printf("שר<             ½s¼g×ּ:×פ«a³ֽ                        >שר\n");
+	printf("שרvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvשר\n");
+	printf("שר                                                    שר\n");
+	printf("שר                                                    שר\n");
+	printf("שר                                                    שר\n");
+	printf("שר                             «צ₪U¥פ·Nֱה¶}©l¨ֿ¥־ ....שר\n");
+	printf("שר                                                    שר\n");
+	printf("שדשששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששו\n");
+	system("pause");
+		//¿י₪J±K½X¡A¦p×G¿ש»~₪T¦¸«hµ²§פµ{¦¡ 
+	while(1){
+		if(b==3){
+			printf("\a");
+			printf("±K½X¿ש»~₪T¦¸");
+			return 0;
+		}
+		printf("½׀¿י₪J±K½X:");
+		scanf("%d",&a);
+		if(a==2025)
+			
+			break;
+		else
+			b++;
+	}
+	
+	system("CLS");
+	//²M°£×©­±
+	//¦p×G±K½X¥¿½T¡Aֵד¥Ü¿ן³ז 
+}
